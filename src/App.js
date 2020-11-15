da41a46a1ae2a323b58b1da37876d169ed6aacd9
RedForest <74250270+RedForest0607@users.noreply.github.com>
2020-11-15 14:38:22 +0900

    11/15
    state공부중

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Component} from 'react';
+//렌더링할 컴포넌트에 다른 컴포넌트 가져와서 렌더링
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//인수로 받은 오브젝트에서 Fav라는 이름의 value를 꺼내옴
+/*
+function Food({name}){
+  foodILike.map(current =>{
+  return <h1>{foodILike.name}</h1>
+  })
 }
+*/
+class App extends Component{
+  //import 컴포넌트를 헤더에서 안해줬다면, react.Component
+  state = {
+    count:0
+  };
+
+  //function 선언
+  add = () => {
+    this.setState(current => ({count: current.count+1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count-1}));
+  };
+  render(){
+    return <div>
+      <h1>the number is : {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      {/*add.eventListener */}
+      <button onClick={this.minus}>Minus</button>
+      </div>;
+  }
+}
+
+
 
 export default App;
